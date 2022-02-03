c840e575d947efb2a158a36f93c2894ba4960681
utam0k <toru@gitpod.io>
2022-02-03 05:57:48 +0000

    update.

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ import (
 
 func main() {
 	runtime.LockOSThread()
-	var syscalls = []string{"mount"}
+	defer runtime.UnlockOSThread()
+
+	// TODO(toru): When I try to apply umount to ActNotify, I encounter the following error.
+	// panic: numerical argument out of domain
+	// var syscalls = []string{"mount", "umount", "umount2", "bind", "chown"}
+	var syscalls = []string{"mount", "umount2", "bind", "chown"}
 	filter, err := libseccomp.NewFilter(libseccomp.ActAllow)
 	if err != nil {
 		panic(err)
 	}
+	err = filter.SetNoNewPrivsBit(false)
+	if err != nil {
+		panic(err)
+	}
+
 	err = filter.SetTsync(false)
 	if err != nil {
 		panic(err)
@@ -29,6 +39,21 @@ func main() {
 			panic(err)
 		}
 	}
-	filter.Load()
+
+	err = filter.Load()
+	// TODO(toru): When I checked usign strace, I didn't have enough permissions for seccomp syscall, but it's unclear why.
+	// panic: operation canceled
+	//
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	fd, err := filter.GetNotifFd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("scmpFd: %w", fd)
+
 	fmt.Println("Finish")
 }
